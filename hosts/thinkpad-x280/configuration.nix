b7bc81fc3fd275c63720a7c45cfe281eed9b7698
Dan Kirkham <daniel.a.kirkham@gmail.com>
2025-05-26 20:09:39 -0500

    Change hostname, finally

--- hosts/thinkpad-x280/configuration.nix
+++ hosts/thinkpad-x280/configuration.nix
@@ -15,7 +15,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "dan-nixos-thinkpad-x280"; # Define your hostname.
+  networking.hostName = "thinkpad-x280"; # Define your hostname.
 
   # Pick only one of the below networking options.
   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
