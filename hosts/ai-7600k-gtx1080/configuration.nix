d6a689002e0371074c0886578a49189eed5e20a6
Dan Kirkham <daniel.a.kirkham@gmail.com>
2025-05-28 06:23:11 -0500

    Enable graphics

--- hosts/ai-7600k-gtx1080/configuration.nix
+++ hosts/ai-7600k-gtx1080/configuration.nix
@@ -3,6 +3,7 @@
   imports = [
     ./hardware-configuration.nix
     ../../shared/configuration.nix
+    ./nvidia.nix
   ];
 
   networking.hostName = "ai-7600k-gtx1080";
@@ -10,24 +11,5 @@
   services.xserver.enable = false;
 
   services.openssh.enable = true;
-
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "nvidia-x11"
-    "nvidia-settings"
-    "nvidia-persistenced"
-  ];
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  # environment.systemPackages = with pkgs; [
-  #   vim
-  # ];
 }
 
---
+++ hosts/ai-7600k-gtx1080/nvidia.nix
@@ -0,0 +1,17 @@
+{ config, lib, pkgs, ... }:
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "nvidia-x11"
+    "nvidia-settings"
+    "nvidia-persistenced"
+  ];
+
+  hardware.graphics.enable = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+}
