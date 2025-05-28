1851cb5e18f55e86cadebd3c92d64db0cbfb9458
Dan Kirkham <daniel.a.kirkham@gmail.com>
2025-05-28 07:51:12 -0500

    unfree

--- hosts/ai-7600k-gtx1080/nvidia.nix
+++ hosts/ai-7600k-gtx1080/nvidia.nix
@@ -2,16 +2,22 @@
 {
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "cuda-merged"
-    "cuda_cccl"
-    "cuda_cudart"
-    "cuda_cuobjdump"
-    "cuda_cupti"
-    "cuda_cuxxfilt"
-    "cuda_gdb"
-    "cuda_nvcc"
-    "cuda_nvdisasm"
-    "cuda_nvml_dev"
-    "cuda_nvprune"
+
+    "cuda_cccl",
+    "cuda_cudart",
+    "cuda_cuobjdump",
+    "cuda_cupti",
+    "cuda_cuxxfilt",
+    "cuda_gdb",
+    "cuda_nvcc",
+    "cuda_nvdisasm",
+    "cuda_nvml_dev",
+    "cuda_nvprune",
+    "cuda_nvrtc",
+    "cuda_nvtx",
+    "cuda_profiler_api",
+    "cuda_sanitizer_api",
+
     "nvidia-persistenced"
     "nvidia-settings"
     "nvidia-x11"
