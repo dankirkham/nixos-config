703827644f55be4bfc1543d6331eb6502f943fae
Dan Kirkham <daniel.a.kirkham@gmail.com>
2025-05-28 06:58:08 -0500

    cuda unfree

--- hosts/ai-7600k-gtx1080/nvidia.nix
+++ hosts/ai-7600k-gtx1080/nvidia.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 {
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "cudatoolkit"
     "nvidia-x11"
     "nvidia-settings"
     "nvidia-persistenced"
