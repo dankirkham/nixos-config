6ce203c67c2d1226c1f72d0655350770c5610901
Dan Kirkham <daniel.a.kirkham@gmail.com>
2025-05-28 06:58:59 -0500

    more unfree

--- hosts/ai-7600k-gtx1080/nvidia.nix
+++ hosts/ai-7600k-gtx1080/nvidia.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 {
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "cuda-merged-12.8"
     "cudatoolkit"
     "nvidia-x11"
     "nvidia-settings"
