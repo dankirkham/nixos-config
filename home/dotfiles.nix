20f3661b8a18bfab8820ae2411ea6668cfd54f78
Dan Kirkham <daniel.a.kirkham@gmail.com>
2025-05-30 21:40:28 -0500

    tmux config

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -3,7 +3,11 @@ let
   home = config.home.homeDirectory;
   configPath = "${home}/.config";
   dotfilesPath = "/etc/nixos/dotfiles";
-  programs = [ "nvim" "wezterm" ];
+  programs = [
+    "nvim"
+    "tmux"
+    "wezterm"
+  ];
 in {
   systemd.user.tmpfiles.rules =
     map (cfg: "L+ ${configPath}/${cfg} - - - - ${dotfilesPath}/${cfg}")
