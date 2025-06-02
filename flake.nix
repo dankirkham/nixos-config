0b3ae473ec538b1936f256b8c5ccf3ba5272d11e
Dan Kirkham <daniel.a.kirkham@gmail.com>
2025-06-01 23:22:07 -0500

    disable rpi

--- flake.nix
+++ flake.nix
@@ -1,20 +1,21 @@
 {
   description = "Dan's nixos flake";
-  nixConfig = {
-    # Only during the first build, otherwise I don't want to allow such a binary cache
-    extra-substituters = [ "https://nix-community.cachix.org" ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
+  # nixConfig = {
+  #   extra-substituters = [
+  #     "https://nixos-raspberrypi.cachix.org"
+  #   ];
+  #   extra-trusted-public-keys = [
+  #     "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+  #   ];
+  # };
   inputs = {
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    raspberry-pi-nix.url = "github:nix-community/raspberry-pi-nix";
+    # nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi";
   };
 
-  outputs = { nixpkgs, home-manager, raspberry-pi-nix, ... }: {
+  outputs = { nixpkgs, home-manager, nixos-raspberrypi, ... }@inputs: {
     nixosConfigurations.thinkpad-x280 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -39,19 +40,34 @@
         }
       ];
     };
-    nixosConfigurations.mmdvm-hotspot = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = [
-        raspberry-pi-nix.nixosModules.raspberry-pi
-        raspberry-pi-nix.nixosModules.sd-image
-        ./hosts/mmdvm-hotspot/configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.dan = ./hosts/mmdvm-hotspot/home.nix;
-        }
-      ];
-    };
+    # nixosConfigurations.mmdvm-hotspot = nixos-raspberrypi.lib.nixosSystemInstaller {
+    #   specialArgs = inputs;
+    #   system = "aarch64-linux";
+    #   modules = [
+    #     {
+    #       imports = with nixos-raspberrypi.nixosModules; [
+    #         raspberry-pi-5.base
+    #       ];
+    #     }
+
+    #     ({ config, ... }: {
+    #       system.nixos.tags = let
+    #         cfg = config.boot.loader.raspberryPi;
+    #       in [
+    #         "raspberry-pi-${cfg.variant}"
+    #         cfg.bootloader
+    #         config.boot.kernelPackages.kernel.version
+    #       ];
+    #     })
+
+    #     ./hosts/mmdvm-hotspot/configuration.nix
+    #     home-manager.nixosModules.home-manager
+    #     {
+    #       home-manager.useGlobalPkgs = true;
+    #       home-manager.useUserPackages = true;
+    #       home-manager.users.dan = ./hosts/mmdvm-hotspot/home.nix;
+    #     }
+    #   ];
+    # };
   };
 }
